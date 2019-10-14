e315fe80dee0ed4d362ee2873607069b0c15242b
Lemonsity <Lemonsity@lemon.com>
2019-10-14 05:36:06 -0400

    Update A2_Checker_V2.py

--- A2_Checker_V2.py
+++ A2_Checker_V2.py
@@ -2,8 +2,10 @@
 # Oct 14, 2019
 # 66 test cases
 
+# designed for elevation.py 2019 fall semester
+
 import unittest
-import elevation as e
+import elevation as e # require elevation.py file
 
 class A2CheckerV2(unittest.TestCase):
     def setUp(self):
@@ -24,7 +26,7 @@ class A2CheckerV2(unittest.TestCase):
                     [60, 93, 67, 99, 37]]
 
     def test_compare(self):
-        print("\ncompare_elevations_within_row:")
+        print("\ncompare_elevations_within_row:", end = " ")
         self.m_x = [[1, 2, 3], [5, 6, 5], [4, 5, 8]]
         self.assertListEqual(e.compare_elevations_within_row(self.m_x, 0, 2), [1, 1, 0], "contain equal")
         self.assertListEqual(e.compare_elevations_within_row(self.m_x, 1, 6), [2, 1, 0], "contain equal")
@@ -33,7 +35,7 @@ class A2CheckerV2(unittest.TestCase):
         self.assertListEqual(e.compare_elevations_within_row(self.m_3, 1, 10), [2, 0, 1], "simple")
 
     def test_update(self):
-        print("\nupdate_elevation:")
+        print("\n\nupdate_elevation:", end = " ")
         e.update_elevation(self.m_3, [0, 1], [2, 1], 1)
         self.assertListEqual(self.m_3, [[11, 13, 16], [9, 9, 14], [4, 28, 2]], "vertical entire")
         e.update_elevation(self.m_3, [0, 0], [1, 0], 1)
@@ -54,7 +56,7 @@ class A2CheckerV2(unittest.TestCase):
         self.assertListEqual(self.m_5, [[58, 102, 45, 96, 115], [70, 7, 56, 31, 55], [107, 1077, 1052, 1089, 91], [35, 39, 20, 16, 112], [60, 92, 67, 99, 47]], "edge case one spot")
 
     def test_average(self):
-        print("\nget_average_elevation:")
+        print("\n\nget_average_elevation:", end = " ")
         self.assertAlmostEqual(e.get_average_elevation(self.m_1), 1, 3, "simple")
         self.assertAlmostEqual(e.get_average_elevation(self.m_2), 2.75, 3, "simple")
         self.assertAlmostEqual(e.get_average_elevation(self.m_3), 11.4444, 3, "simple")
@@ -62,7 +64,7 @@ class A2CheckerV2(unittest.TestCase):
         self.assertAlmostEqual(e.get_average_elevation(self.m_5), 65.32, 3, "simple")
 
     def test_peak(self):
-        print("\nfind_peak:")
+        print("\n\nfind_peak:", end = " ")
         self.assertListEqual(e.find_peak(self.m_1), [0, 0], "simple")
         self.assertListEqual(e.find_peak(self.m_2), [1, 0], "simple")
         self.assertListEqual(e.find_peak(self.m_3), [2, 1], "simple")
@@ -70,7 +72,7 @@ class A2CheckerV2(unittest.TestCase):
         self.assertListEqual(e.find_peak(self.m_5), [0, 4], "simple")
 
     def test_is_sink(self):
-        print("\nis_sink:")
+        print("is_sink:", end = " ")
         self.assertTrue(e.is_sink(self.m_1, [0, 0]), "edge case no surrounding")
         self.assertFalse(e.is_sink(self.m_1, [1, 1]), "out of bound")
         self.assertFalse(e.is_sink(self.m_2, [0, 0]), "special case diagonal")
@@ -86,7 +88,7 @@ class A2CheckerV2(unittest.TestCase):
         self.assertFalse(e.is_sink(self.m_5, [3, 2]), "simple")
 
     def test_local_sink(self):
-        print("\nfind_local_sink:")
+        print("\n\nfind_local_sink:", end = " ")
         self.assertListEqual(e.find_local_sink(self.m_1, [0, 0]), [0, 0], "edge case no surrounding")
         self.assertListEqual(e.find_local_sink(self.m_2, [1, 1]), [1, 1], "edge case already sink")
         self.assertListEqual(e.find_local_sink(self.m_2, [0, 0]), [1, 1], "simple diagonal")
@@ -98,7 +100,7 @@ class A2CheckerV2(unittest.TestCase):
         self.assertListEqual(e.find_local_sink(self.m_5, [2, 3]), [3, 3], "simple")
 
     def test_hike(self):
-        print("\ncan_hike_to:")
+        print("can_hike_to:", end = " ")
         self.assertTrue(e.can_hike_to(self.m_3, [2, 2], [0, 2], 100), "simple")
         self.assertTrue(e.can_hike_to(self.m_3, [2, 2], [0, 2], 14), "edge case end with 0")
         self.assertFalse(e.can_hike_to(self.m_3, [2, 2], [0, 2], 13), "simple not enough")
@@ -119,7 +121,7 @@ class A2CheckerV2(unittest.TestCase):
         self.assertFalse(e.can_hike_to(self.m_x, [2, 2], [0, 0], 10), "simple not enough")
 
     def test_resolution(self):
-        print("\nget_lower_resolution")
+        print("\n\nget_lower_resolution", end = " ")
         self.assertListEqual(e.get_lower_resolution(self.m_1), [[1]], "odd size")
         self.assertListEqual(e.get_lower_resolution(self.m_2), [[2]], "simple")
         self.assertListEqual(e.get_lower_resolution(self.m_3), [[10, 15], [15, 2]], "odd size")
